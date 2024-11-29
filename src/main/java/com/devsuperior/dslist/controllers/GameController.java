8787432e1c1671d6dae4849512fd1e48830af7b5
vitorbnr <vitorbernardotech@gmail.com>
2024-11-29 14:13:32 -0300

    Updates

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -2,7 +2,6 @@ package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
